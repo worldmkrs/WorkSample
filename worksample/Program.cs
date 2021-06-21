0a719741255a30ca9ead07acb623dfb236bb21f7
worldmkrs <86087114+worldmkrs@users.noreply.github.com>
2021-06-20 20:34:41 -0500

    program.cs file updated
    made some additional changes to existing code.

---
+++ worksample/Program.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace InMemoryDictionary
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            while (true)
+            {
+                var command = Console.ReadLine();
+                var commands = command.Split(' ');
+                switch (commands[0])
+                {
+                    case "ADD":
+                        if (commands.Length == 3)
+                        {
+                            MyStore.Add(commands[1], commands[2]);
+                        }
+                        break;
+                    case "REMOVE":
+                        if (commands.Length == 3)
+                        {
+                            MyStore.Remove(commands[1], commands[2]);
+                        }
+                        break;
+                    case "REMOVEALL":
+                        if (commands.Length == 2)
+                        {
+                            MyStore.RemoveAll(commands[1]);
+                        }
+                        break;
+                    case "CLEAR":
+                        MyStore.Clear();
+                        break;
+                    case "KEYS":
+                        MyStore.Keys();
+                        break;
+                    case "MEMBERS":
+                        MyStore.Members();
+                        break;
+                    case "KEYEXISTS":
+                        if (commands.Length == 2)
+                        {
+                            MyStore.KeyExist(commands[1]);
+                        }
+                        break;
+                    case "ALLMEMBERS":
+                        MyStore.AllMembers();
+                        break;
+                    case "ITEMS":
+                        MyStore.Items();
+                        break;
+
+                }
+            }
+        }
+    }
+
+    public class MyStore
+    {
+        public static List<KeyStore> data = new List<KeyStore>();
+        public static Dictionary<string, List<string>> ddata = new Dictionary<string, List<string>>();
+        public static void Add(string key, string value)
+        {
+            if (ddata.ContainsKey(key))
+            {
+                if (ddata[key].Contains(value))
+                {
+
+                    Print("Error");
+                }
+            }
+            data.Add(new KeyStore(key, value));
+            Print("Added");
+        }
+
+        public static void Remove(string key, string value)
+        {
+            if (data.Any(x => x.Key == key && x.Value == value))
+            {
+                data.RemoveAll(x => x.Key == key && x.Value == value);
+                Print("Removed");
+            }
+            else
+            {
+                Print("ERROR, member does not exist");
+            }
+        }
+
+        public static void Clear()
+        {
+            data.Clear();
+            Print("Removed");
+        }
+
+        public static void KeyExist(string key)
+        {
+            Print(data.Any(x => x.Key == key).ToString());
+        }
+
+        public static void RemoveAll(string member)
+        {
+            if (data.Any(x => x.Key == member))
+            {
+                data.RemoveAll(x => x.Key == member);
+                Print("Removed");
+            }
+            else
+            {
+                Print("ERROR, key does not exist");
+            }
+        }
+
+        public static void Keys()
+        {
+            var keys = data.Select(x => x.Key).ToList();
+            var index = 1;
+            foreach (var v in data)
+            {
+                Print(v.Key);
+            }
+            if (keys.Any())
+            {
+                keys.ForEach(x =>
+                {
+                    Print($"{index}) {x}");
+
+                    index++;
+                });
+            }
+            else
+            {
+                Print("(empty set)");
+            }
+        }
+        public static void Members()
+        {
+            var members = data.Select(x => x.Value).ToList();
+            var index = 1;
+            foreach (var v in data)
+            {
+                Print(v.Value);
+            }
+            if (members.Any())
+            {
+                members.ForEach(x =>
+                {
+                    Print($"{index}) {x}");
+                    index++;
+                });
+            }
+            else
+            {
+                Print("(empty set)");
+            }
+        }
+        public static void Items()
+        {
+            var items = data.Select(x => "{ x.Key}: { x.Value}").ToList();
+            var index = 1;
+            foreach (var v in data)
+            {
+                Print(v.Key);
+            }
+            if (items.Any())
+            {
+                items.ForEach(x =>
+                {
+                    Print($"{index}) {x}");
+
+                    index++;
+                });
+            }
+            else
+            {
+                Print("(empty set)");
+            }
+        }
+
+        public static void AllMembers()
+        {
+            var members = data.Select(x => x.Value).ToList();
+            var index = 1;
+            foreach (var v in data)
+            {
+                Print(v.Value);
+            }
+            if (members.Any())
+            {
+                members.ForEach(x =>
+                {
+                   Print($"{index}) {x}");
+
+                    index++;
+                });
+            }
+            else
+            {
+                Print("(empty set)");
+            }
+        }
+
+        static void Print(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+    public class KeyStore
+    {
+        public KeyStore(string key, string value)
+        {
+            Key = key;
+            Value = value;
+        }
+        public string Key { get; set; }
+        public string Value { get; set; }
+        public object Member { get; internal set; }
+    }
+
+}
